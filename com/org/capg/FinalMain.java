dff346a91441057f0731b816b42d183da159735c
lkvreddy007 <lkvreddy007@gmail.com>
2020-10-02 12:57:42 +0530

    [Krishna]added-abilityToSearchPersonInACityOrStateAcrossMultipleAddressBooks

--- com/org/capg/FinalMain.java
+++ com/org/capg/FinalMain.java
@@ -1,18 +1,35 @@
 package com.org.capg;
 
 import java.util.*;
+import java.util.stream.Collectors;
+
 public class FinalMain {
+	static AddressBookMain abm=new AddressBookMain();
+	static HashMap<String, List<Contact>> shelf=new HashMap<>();
 	private static List<Contact> addressBook=new ArrayList<Contact>();
 	static Scanner sc=new Scanner(System.in);
+	
+	public static void searchPersonInAState(String city){
+		List<Contact> matchingobj=new ArrayList<Contact>();
+		for(List<Contact> li:shelf.values()) {
+			matchingobj=li.stream()
+				  .filter(a->a.getAddress().equals(city))
+				  .collect(Collectors.toList());
+		}
+		System.out.println("Contacts from "+city+" are :");
+		for(Contact c:matchingobj) {
+			System.out.println(c.getFirstName()+c.getLastName());
+		}
+	}
+	
 	public static void main(String[] args) {
-		AddressBookMain abm=new AddressBookMain();
-		HashMap<String, List<Contact>> shelf=new HashMap<>();
-		boolean exit =true;
+		boolean exit = true;
 		while(exit) {
 			System.out.println("\nMenu");
 			System.out.println("1.Add Address Book to Shelf");
 			System.out.println("2.Display names of all address books in shelf");
-			System.out.println("3.Exit");
+			System.out.println("3.Display contacts by City/State across multiple Address Books");
+			System.out.println("4.Exit");
 			System.out.println("Enter your choice");
 			int check=Integer.parseInt(sc.nextLine());
 			switch(check){
@@ -43,6 +60,11 @@ public class FinalMain {
 					break;
 					
 				case 3:
+					System.out.println("Enter City name to search contacts");
+					String city= sc.nextLine();
+					searchPersonInAState(city);
+					break;
+				case 4:
 					exit=false;
 					break;
 					
